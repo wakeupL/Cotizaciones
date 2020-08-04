e99297fd8579073cd8869094f29a7b14dd678a45
Angel Oyarzun <deagleeanzun@gmail.com>
2020-08-03 21:38:50 -0400

    controladores, migraciones y modelos creados

---
+++ database/migrations/2020_08_04_012022_create_cotizacion_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCotizacionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cotizacion', function (Blueprint $table) {
+            $table->id();
+            $table->string('cliente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cotizacion');
+    }
+}
---
+++ database/migrations/2020_08_04_012058_create_productos_cotizacion_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosCotizacionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos_cotizacion', function (Blueprint $table) {
+            $table->id();
+            $table->string('producto');
+            $table->string('cantidad');
+            $table->string('precio');
+            $table->string('total_neto');
+            $table->string('entrega');
+            $table->string('id_cotizacion');
+            $table->timestamps();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos_cotizacion');
+    }
+}
